fd26a9fa68e739a8f9f070f789deecee41f48ddb
alikov321-sys <alikov321@gmail.com>
2026-01-29 00:23:29 +0500

    add lab1

---
+++ lab/14.py
@@ -0,0 +1,4 @@
+a=input()
+b=int(input())
+
+print(f"Hello,{a}.You are {b} years old.")
---
+++ lab/15.py
@@ -0,0 +1,7 @@
+a=input()
+b=input()
+for i in a:
+    if i==b:
+        print(True)
+    else:
+        print(False)
